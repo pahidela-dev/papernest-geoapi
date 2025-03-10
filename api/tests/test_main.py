ff8b21a5607f8fa4c3e91599ec96a28e8312a84c
pahidela <pahidela.dev@outlook.com>
2025-03-10 21:20:13 +0100

    Add example test

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -7,3 +7,11 @@ def test_api_working():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+def test_example_search():
+    response = client.get("search/?q=42+rue+papernest+75011+Paris")
+    assert response.status_code == 200
+    assert response.json() == {
+        "orange": {"2G": True, "3G": True, "4G": False}, 
+        "SFR": {"2G": True, "3G": True, "4G": True}
+    }
